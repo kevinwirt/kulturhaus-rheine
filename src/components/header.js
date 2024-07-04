e75a759e9fb872ae8ba32c49f535ae20aec29b2a
kevinwirt <kevin.wirt07@gmail.com>
2024-07-04 10:57:34 +0200

    Sprachenschalter außerhalb vom Burgermenü

--- src/components/header.js
+++ src/components/header.js
@@ -10,45 +10,45 @@ const Header = ({ scrollToSection }) => {
 
   return (
     <div className="header">
-      <div className="scroll-buttons">
-        <button className="button" onClick={() => scrollToSection('aktuellesRef')}>
-          Aktuelles
-        </button>
-        <button className="button" onClick={() => scrollToSection('eventsRef')}>
-          Events
-        </button>
-        <button className="button" onClick={() => scrollToSection('kurseRef')}>
-          Kurse
-        </button>
-        <button className="button" onClick={() => scrollToSection('kontaktRef')}>
-          Kontakt
-        </button>
+      <div className="left-section">
+        <div className="scroll-buttons">
+          <button className="button" onClick={() => scrollToSection('aktuellesRef')}>
+            Aktuelles
+          </button>
+          <button className="button" onClick={() => scrollToSection('eventsRef')}>
+            Events
+          </button>
+          <button className="button" onClick={() => scrollToSection('kurseRef')}>
+            Kurse
+          </button>
+          <button className="button" onClick={() => scrollToSection('kontaktRef')}>
+            Kontakt
+          </button>
+        </div>
+        <div className="burger-menu" onClick={toggleMenu}>
+          &#9776;
+        </div>
+        {menuOpen && (
+          <div className="mobile-menu">
+            <button className="button" onClick={() => { scrollToSection('aktuellesRef'); toggleMenu(); }}>
+              Aktuelles
+            </button>
+            <button className="button" onClick={() => { scrollToSection('eventsRef'); toggleMenu(); }}>
+              Events
+            </button>
+            <button className="button" onClick={() => { scrollToSection('kurseRef'); toggleMenu(); }}>
+              Kurse
+            </button>
+            <button className="button" onClick={() => { scrollToSection('kontaktRef'); toggleMenu(); }}>
+              Kontakt
+            </button>
+          </div>
+        )}
       </div>
       <div className="language-buttons">
         <button className="button">DE</button>
         <button className="button">EN</button>
       </div>
-      <div className="burger-menu" onClick={toggleMenu}>
-        &#9776;
-      </div>
-      {menuOpen && (
-        <div className="mobile-menu">
-          <button className="button" onClick={() => { scrollToSection('aktuellesRef'); toggleMenu(); }}>
-            Aktuelles
-          </button>
-          <button className="button" onClick={() => { scrollToSection('eventsRef'); toggleMenu(); }}>
-            Events
-          </button>
-          <button className="button" onClick={() => { scrollToSection('kurseRef'); toggleMenu(); }}>
-            Kurse
-          </button>
-          <button className="button" onClick={() => { scrollToSection('kontaktRef'); toggleMenu(); }}>
-            Kontakt
-          </button>
-          <button className="button">DE</button>
-          <button className="button">EN</button>
-        </div>
-      )}
     </div>
   );
 }
